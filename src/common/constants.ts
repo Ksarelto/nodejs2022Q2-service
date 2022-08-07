a922ea8225985fda09ebc1d2321331ef2ad7a66f
Artsiom Murashko <ksarelto@tut.by>
2022-08-07 20:39:29 +0300

    [develop-logging] Add logger

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -16,4 +16,26 @@ const databaseType = 'postgres';
 
 const passwordStrength = 7;
 
+export const LogLevelsObject = {
+  crit: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  http: 4,
+};
+
+export const LogColorsObject = {
+  crit: 'white redBG',
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+};
+
+export const defaultAdmin = {
+  name: 'Admin',
+  login: 'admin',
+  password: 'admin',
+};
+
 export { errorMessage, databaseType, passwordStrength };
